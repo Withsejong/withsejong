fc1e96b5ebf193de43d5624dd209f6dba8046f29
MisterJerry <misterjerry@sju.ac.kr>
2024-02-26 20:10:14 +0900

    init commit

--- app/src/main/java/com/withsejong/MainActivity.kt
+++ app/src/main/java/com/withsejong/MainActivity.kt
@@ -27,20 +27,21 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WithSejongTheme {
-        Greeting("Android")
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        WithSejongTheme {
+            Greeting("Android")
+        }
     }
 }
