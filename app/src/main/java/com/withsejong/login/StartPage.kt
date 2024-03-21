8f8b0dd563db72fca52f3e8848477451c7ab63d6
Kimtaeshik <kts11073220@gmail.com>
2024-03-22 01:44:47 +0900

    start_page_30%

---
+++ app/src/main/java/com/withsejong/login/StartPage.kt
@@ -0,0 +1,12 @@
+package com.withsejong.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.withsejong.R
+
+class StartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
