9ec31fc2999538e0f4781f7189d08210fe407684
MisterJerry <misterjerry@sju.ac.kr>
2024-03-21 14:00:01 +0900

    로그인 페이지 중간 완성

---
+++ app/src/main/java/com/withsejong/chatting_prototype/Prototype01.kt
@@ -0,0 +1,12 @@
+package com.withsejong.chatting_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.withsejong.R
+
+class Prototype01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prototype01)
+    }
+}
